ffa7e5522b34b908c7d26e25b6d81e6719c15261
tsi1622 <tayu.16.enye@gmail.com>
2025-07-17 23:23:18 +0300

    chore: configure Vite to run on port 3000

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,13 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: 3000, 
   },
 })
